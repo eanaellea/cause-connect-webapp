e817e6af8e796202f80262b4ee8e8f3a9bc149da
aea <anaelle3364@gmail.com>
2024-03-26 11:53:36 +0100

    feat(landing page): add features section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,36 +1,37 @@
 import { FC } from 'react'
-import styles from './Home.module.scss'
 import { Button, Card } from 'antd'
 import { UserOutlined, CalendarOutlined, CreditCardOutlined, FileOutlined } from '@ant-design/icons'
+
+import styles from './Home.module.scss'
+
 const { Meta } = Card;
 
-export const Home = () => {
-  const features = [
-    {
-      title: 'Gestion des membres',
-      description: 'Gérez les membres de votre association, ajoutez, modifiez, supprimez des membres en quelques clics.',
-      img: <UserOutlined className={styles.featureIcon} />,
-    },
-    {
-      title: 'Gestion des événements',
-      description: 'Créez des événements, invitez des membres, gérez les inscriptions, et bien plus.',
-      img: <CalendarOutlined className={styles.featureIcon} />,
-    },
-    {
-      title: 'Gestion des cotisations',
-      description: 'Créez des cotisations, suivez les paiements, et générez des reçus fiscaux.',
-      img: <CreditCardOutlined className={styles.featureIcon} />,
-    },
-    {
-      title: 'Gestion des documents',
-      description: 'Partagez des documents, créez des dossiers, et gérez les droits d\'accès.',
-      img: <FileOutlined className={styles.featureIcon} />,
-    },
-  ]
+const features = [
+  {
+    title: 'Gestion des membres',
+    description: 'Gérez les membres de votre association, ajoutez, modifiez, supprimez des membres en quelques clics.',
+    img: <UserOutlined className={styles.featureIcon} />,
+  },
+  {
+    title: 'Gestion des événements',
+    description: 'Créez des événements, invitez des membres, gérez les inscriptions, et bien plus.',
+    img: <CalendarOutlined className={styles.featureIcon} />,
+  },
+  {
+    title: 'Gestion des cotisations',
+    description: 'Créez des cotisations, suivez les paiements, et générez des reçus fiscaux.',
+    img: <CreditCardOutlined className={styles.featureIcon} />,
+  },
+  {
+    title: 'Gestion des documents',
+    description: 'Partagez des documents, créez des dossiers, et gérez les droits d\'accès.',
+    img: <FileOutlined className={styles.featureIcon} />,
+  },
+]
 
 export const Home: FC = () => {
   return (
-    <>
+    <main className={styles.main} >
       <span className={styles.background}></span>
       <section className={styles.gettingStarted}>
         <h2>La gestion de votre association, simplifiée</h2>
@@ -50,6 +51,6 @@ export const Home: FC = () => {
           ))}
         </div>
       </section>
-    </>
+    </main>
   )
 }
