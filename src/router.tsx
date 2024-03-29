fb141e166b613eb7f40fae44a08b69c356743bee
Paul Gaudeaux <phvcgdx@gmail.com>
2024-03-29 10:18:10 +0100

    feat(auth): make auth work

--- src/router.tsx
+++ src/router.tsx
@@ -5,48 +5,54 @@ import { Home } from './pages/home/Home'
 import { Register } from './pages/register/Register'
 import { ForgottenPassword } from './pages/forgottenPassword/ForgottenPassword'
 import { LogIn } from './pages/logIn/LogIn'
+import { useGlobalStore } from './store/store'
+import { Dashboard } from './pages/dashboard/Dashboard'
+import { ResetPassword } from './pages/resetPassword/ResetPassword'
 import { PublicLayout } from './pages/publicLayout/PublicLayout'
 
 export const router = createBrowserRouter(
   [
     {
-      path: '/app',
+      path: '/',
       element: <GlobalLayout />,
-      loader: () => {
-        // TODO: implement auth check
-        if (localStorage.getItem('token') === null) {
-          return redirect('/')
-        }
-        return null
-      },
       children: [
+        {
+          path: '',
+          element: <PublicLayout />,
+          children: [
+            {
+              path: '',
+              element: <Home />
+            },
+            {
+              path: 'login',
+              element: <LogIn />
+            },
+            {
+              path: 'register',
+              element: <Register />
+            },
+            {
+              path: 'forgotten-password',
+              element: <ForgottenPassword />
+            },
+            {
+              path: 'reset-password',
+              element: <ResetPassword />
+            }
+          ]
+        },
         {
           path: '/app',
-          element: <Home />
-        }
-      ],
-    },
-    {
-      path: '/',
-      element: <PublicLayout />,
-      children: [
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/login',
-          element: <LogIn />
-        },
-        {
-          path: '/register',
-          element: <Register />
-        },
-        {
-          path: '/forgotten-password',
-          element: <ForgottenPassword />
+          loader: () => {
+            if (useGlobalStore.getState().token === null) {
+              return redirect('/login')
+            }
+            return null
+          },
+          element: <Dashboard />
         }
       ]
     }
-  ],
+  ]
 )
--- src/services/mainApi/setup/helpers.ts
+++ src/services/mainApi/setup/helpers.ts
@@ -1,6 +1,6 @@
-export function handleError(e: Error) {
-  console.log("error", e);
-  if (e.message === "Network request failed") {
+export function handleError (e: Error): void {
+  console.log('error', e)
+  if (e.message === 'Network request failed') {
     // Toast.show({ type: "error", text1: "We can't talk to the server" });
   } else {
     // Toast.show({ type: "error", text1: e?.message ?? "An error occured" });
