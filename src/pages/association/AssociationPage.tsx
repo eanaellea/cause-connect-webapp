81804df44a9bfbbc55e19faa806f957cf68f890e
aea <anaelle3364@gmail.com>
2024-04-01 21:11:09 +0200

    refac(linter): clean files

--- src/pages/association/AssociationPage.tsx
+++ src/pages/association/AssociationPage.tsx
@@ -1,18 +1,17 @@
-import { useGlobalStore } from "@/store/store";
-import { FC } from "react";
-import { Alert } from 'antd';
+import { useGlobalStore } from '@/store/store'
+import { FC } from 'react'
+import { Alert } from 'antd'
 
-import { AssociationCard } from "@/designSystem/associationProfile/AssociationCard";
+import { AssociationCard } from '@/designSystem/associationProfile/AssociationCard'
 import styles from './AssociationPage.module.scss'
 
 export const AssociationPage: FC = () => {
-
   const association = useGlobalStore(state => state.association!)
 
   return (
     <div className={styles.profile}>
       <h1>Informations de l'association</h1>
-      <Alert message="Le changement du logo ne demande aucune confirmation." type="info" showIcon/>
+      <Alert message='Le changement du logo ne demande aucune confirmation.' type='info' showIcon />
       <AssociationCard association={association} />
     </div>
   )
