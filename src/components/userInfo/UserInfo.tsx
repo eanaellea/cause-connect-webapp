b259089215844ec44dad3eee611254cb0b8dbe21
aea <anaelle3364@gmail.com>
2024-04-02 18:45:00 +0200

    feat(users): create component to invite user

---
+++ src/components/userInfo/UserInfo.tsx
@@ -0,0 +1,75 @@
+import { FC } from 'react'
+import { Button, Input } from 'antd'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+
+import styles from './UserInfo.module.scss'
+
+const inviteUserSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email()
+})
+
+export const UserInfo: FC = () => {
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<z.infer<typeof inviteUserSchema>>({
+    resolver: zodResolver(inviteUserSchema)
+  })
+  const onSubmit: SubmitHandler<z.infer<typeof inviteUserSchema>> = (data) => {
+    console.log('Sumitting', data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.updateAssocitaionForm}> {/* eslint-disable-line @typescript-eslint/no-misused-promises */}
+      <div className={styles.formContent}>
+        <div className={styles.formControl}>
+          <label>Prénom de l'utilisateur</label>
+          <Controller
+            name='firstName'
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder='Jean'
+              />
+            )}
+          />
+          {(errors.firstName != null) && <span>{errors.firstName.message}</span>}
+        </div>
+        <div className={styles.formControl}>
+          <label>Nom de l'utilisateur</label>
+          <Controller
+            name='lastName'
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder='Bombeur'
+              />
+            )}
+          />
+          {(errors.lastName != null) && <span>{errors.lastName.message}</span>}
+        </div>
+        <div className={styles.formControl}>
+          <label>Adresse e-mail de l'utilisateur</label>
+          <Controller
+            name='email'
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder='user@email.com'
+              />
+            )}
+          />
+          {(errors.email != null) && <span>{errors.email.message}</span>}
+        </div>
+      </div>
+
+      <Button type='primary' htmlType='submit' disabled={!isValid}>
+        Inviter l'utilisateur
+      </Button>
+    </form>
+  )
+}
--- src/pages/association/AssociationPage.tsx
+++ src/pages/association/AssociationPage.tsx
@@ -4,15 +4,25 @@ import { Alert } from 'antd'
 
 import { AssociationCard } from '@/designSystem/associationProfile/AssociationCard'
 import styles from './AssociationPage.module.scss'
+import { UserInfo } from '@/components/userInfo/UserInfo'
 
 export const AssociationPage: FC = () => {
   const association = useGlobalStore(state => state.association!)
 
   return (
-    <div className={styles.profile}>
-      <h1>Informations de l'association</h1>
-      <Alert message='Le changement du logo ne demande aucune confirmation.' type='info' showIcon />
-      <AssociationCard association={association} />
-    </div>
+    <>
+      <h1>Association</h1>
+      <span className={styles.container}>
+        <div className={styles.profile}>
+          <h2>Informations</h2>
+          <Alert message='Le changement du logo ne demande aucune confirmation.' type='info' showIcon />
+          <AssociationCard association={association} />
+        </div>
+        <div className={styles.invite}>
+          <h2>Invitation</h2>
+          <UserInfo />
+        </div>
+      </span>
+    </>
   )
 }
