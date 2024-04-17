f5153c7e4ad67014472bfb6dadd61d2042eb7942
aea <anaelle3364@gmail.com>
2024-04-17 15:53:49 +0200

    fix(image upload): make empty by default

--- src/designSystem/ImageUpload.tsx
+++ src/designSystem/ImageUpload.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from 'react';
-import { Modal, Upload } from 'antd';
+import { FC, useState } from 'react'
+import { Modal, Upload } from 'antd'
 import type { GetProp, UploadFile, UploadProps } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 
@@ -11,7 +11,7 @@ interface ImageUploadProps {
 type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0]
 
 const getBase64 = async (file: FileType): Promise<string> =>
-  new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onload = () => resolve(reader.result as string)
@@ -19,6 +19,7 @@ const getBase64 = async (file: FileType): Promise<string> =>
   })
 
 export const ImageUpload: FC<ImageUploadProps> = ({ initialImage, onChange }) => {
+  console.log('initialImage', initialImage)
   const [previewOpen, setPreviewOpen] = useState(false)
   const [previewImage, setPreviewImage] = useState('')
   const [previewTitle, setPreviewTitle] = useState('')
@@ -29,45 +30,42 @@ export const ImageUpload: FC<ImageUploadProps> = ({ initialImage, onChange }) =>
     url: initialImage
   }])
 
-  const handleCancel = () => setPreviewOpen(false)
+  const handleCancel = (): void => setPreviewOpen(false)
 
-  const handlePreview = async (file: UploadFile) => {
-    if (!file.url && !file.preview) {
+  const handlePreview = async (file: UploadFile): Promise<void> => {
+    if (file.url == null && file.preview == null) {
       file.preview = await getBase64(file.originFileObj as FileType)
     }
 
-    setPreviewImage(file.url || (file.preview as string))
+    setPreviewImage(file.url ?? (file.preview as string))
     setPreviewOpen(true)
-    setPreviewTitle(file.name || file.url!.substring(file.url!.lastIndexOf('/') + 1))
-  };
+    setPreviewTitle(file.name ?? file.url!.substring(file.url!.lastIndexOf('/') + 1)) // eslint-disable-line @typescript-eslint/no-non-null-assertion
+  }
 
   const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
     setFileList(newFileList)
     const file = newFileList[0]?.originFileObj as File
-    if (file) {
+    if (file != null) {
       onChange(file)
     }
   }
-
-  const uploadButton = (
-    <button style={{ border: 0, background: 'none' }} type='button'>
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Importer</div>
-    </button>
-  )
 
   return (
     <>
       <Upload
         beforeUpload={() => false}
         listType='picture-circle'
-        fileList={fileList}
-        onPreview={handlePreview}
+        defaultFileList={initialImage !== undefined || initialImage === '' ? fileList : []}
+        onPreview={handlePreview} // eslint-disable-line @typescript-eslint/no-misused-promises
         onChange={handleChange}
         accept='image/png, image/jpeg, image/jpg'
         className='imageUpload'
+        maxCount={1}
       >
-        {fileList.length >= 1 ? null : uploadButton}
+        <button style={{ border: 0, background: 'none' }} type='button'>
+          <PlusOutlined />
+          <div style={{ marginTop: 8 }}>Importer</div>
+        </button>
       </Upload>
       <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
         <img alt='example' style={{ width: '100%' }} src={previewImage} />
