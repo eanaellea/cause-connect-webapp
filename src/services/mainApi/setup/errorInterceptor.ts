533a1788c0d0f12ff88dbcaf8ba333c65c4b0a5d
aea <anaelle3364@gmail.com>
2024-04-18 09:29:41 +0200

    fix(UX): remove toast for payment error

--- src/services/mainApi/setup/errorInterceptor.ts
+++ src/services/mainApi/setup/errorInterceptor.ts
@@ -1,7 +1,10 @@
 import { AfterResponseHook } from 'ky'
 import toast from 'react-hot-toast'
 
-export const errorInterceptor: AfterResponseHook = async (_, __, response: Response): Promise<Response> => {
+export const errorInterceptor: AfterResponseHook = async (request: Request, _, response: Response): Promise<Response> => {
+  if (request.url.includes('settings/payment')) {
+    return response
+  }
   if (response.status >= 400) {
     const message = (await response.json()).message
     toast.error(message)
